bcee5a1b626badb3d95477ab85cdc4ced6ba8ec1
Johnathan C <28884578+luig0@users.noreply.github.com>
2023-03-18 14:18:08 -0500

    updated api:login to use standard server messages

--- src/lib/Messages.ts
+++ src/lib/Messages.ts
@@ -1,3 +1,4 @@
+export const ACCESS_DENIED = 'Access Denied';
 export const BAD_REQUEST = 'Bad Request';
 export const INTERNAL_SERVER_ERROR = 'Internal Server Error';
 export const METHOD_NOT_ALLOWED = 'Method Not Allowed';
--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -2,6 +2,7 @@ import bcrypt from 'bcrypt';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 import * as db from '../../lib/database/AppDAO';
+import * as messages from '../../lib/Messages';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
@@ -14,18 +15,18 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     try {
       const dbUser = await db.get('SELECT * FROM users WHERE username=?;', [username]);
 
-      if (!dbUser) return res.status(401).send('Access Denied');
+      if (!dbUser) return res.status(401).send(messages.ACCESS_DENIED);
 
       const result = await bcrypt.compare(password, dbUser.password_hash);
 
-      if (!result) return res.status(401).send('Access Denied');
+      if (!result) return res.status(401).send(messages.ACCESS_DENIED);
 
       return res.status(200).send('OK');
     } catch (err) {
       console.log('login.ts error:', err);
-      return res.status(500).send('Internal Server Error');
+      return res.status(500).send(messages.INTERNAL_SERVER_ERROR);
     }
   } else {
-    return res.status(405).send('Method Not Allowed');
+    return res.status(405).send(messages.METHOD_NOT_ALLOWED);
   }
 }
