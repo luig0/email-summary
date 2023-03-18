ab35c39aad68ae7cb7b93af037b62f6c108da050
Johnathan C <28884578+luig0@users.noreply.github.com>
2023-03-18 14:16:48 -0500

    added standard server messages

---
+++ src/lib/Messages.ts
@@ -0,0 +1,6 @@
+export const BAD_REQUEST = 'Bad Request';
+export const INTERNAL_SERVER_ERROR = 'Internal Server Error';
+export const METHOD_NOT_ALLOWED = 'Method Not Allowed';
+
+export const REGISTRATION_SUCCESSFUL = 'Registration Successful';
+export const USERNAME_ALREADY_IN_USE = 'Username Already in Use';
