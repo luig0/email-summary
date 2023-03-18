92362965439745e6ff4da4c6088802f92b5f0421
Johnathan C <28884578+luig0@users.noreply.github.com>
2023-03-18 14:30:42 -0500

    updated username taken message

--- src/lib/Messages.ts
+++ src/lib/Messages.ts
@@ -4,4 +4,4 @@ export const INTERNAL_SERVER_ERROR = 'Internal Server Error';
 export const METHOD_NOT_ALLOWED = 'Method Not Allowed';
 
 export const REGISTRATION_SUCCESSFUL = 'Registration Successful';
-export const USERNAME_ALREADY_IN_USE = 'Username Already in Use';
+export const USERNAME_ALREADY_TAKEN = 'Username Already Taken';
