8483ca646d0c4eca686cfe909ff8a6f37a10d18e
Johnathan C <28884578+luig0@users.noreply.github.com>
2023-03-21 17:05:25 -0500

    wrote SessionReaper.ts script

---
+++ src/scripts/SessionReaper.ts
@@ -0,0 +1,18 @@
+// this file must be executed from project root (joint-summary/)
+
+import * as dao from '../lib/database/AppDAO';
+import * as db from '../lib/database/Adapter';
+
+const CURRENT_TIME = Date.now();
+
+(async () => {
+  const allSessions = await dao.all(`SELECT * FROM sessions`);
+  console.log(`found ${allSessions.length} sessions`);
+
+  for (const session of allSessions) {
+    if (CURRENT_TIME - new Date(session.expires_at).getTime() > 0) {
+      await db.deleteSession(session.session_token);
+      console.log('deleted:', session);
+    }
+  }
+})();
